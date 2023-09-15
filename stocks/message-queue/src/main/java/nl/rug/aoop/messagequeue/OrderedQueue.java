1a476c0feabe7d74fe458c5711be0cc8553352ca
ohassaneRUG <ohassan100100@gmail.com>
2023-09-15 11:41:33 +0200

    Finished ordered and unordered queues classes

--- stocks/message-queue/src/main/java/nl/rug/aoop/messagequeue/OrderedQueue.java
+++ stocks/message-queue/src/main/java/nl/rug/aoop/messagequeue/OrderedQueue.java
@@ -1,18 +1,31 @@
 package nl.rug.aoop.messagequeue;
+import java.time.LocalDateTime;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class OrderedQueue implements MessageQueue {
+    private SortedMap<LocalDateTime, Message> queue = new TreeMap();
     @Override
     public void enqueue(Message message) {
-
+        if (message != null) {
+            queue.put(message.getTimestamp(), message); //What happens when times for 2 messages are the same?
+        }
+        throw new IllegalArgumentException("Message cannot be null");
     }
 
     @Override
     public Message dequeue() {
-        return null;
+        if (!queue.isEmpty()) {
+            LocalDateTime earliestTimestamp = queue.firstKey(); //What happens when times for 2 messages are the same?
+            Message message = queue.get(earliestTimestamp);
+            queue.remove(earliestTimestamp);
+            return message;
+        }
+        throw new IllegalArgumentException("Queue has no elements");
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return queue.size();
     }
 }
--- stocks/message-queue/src/main/java/nl/rug/aoop/messagequeue/UnorderedQueue.java
+++ stocks/message-queue/src/main/java/nl/rug/aoop/messagequeue/UnorderedQueue.java
@@ -1,18 +1,29 @@
 package nl.rug.aoop.messagequeue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UnorderedQueue implements MessageQueue {
+    private Queue<Message> queue = new LinkedList<>();
+
     @Override
     public void enqueue(Message message) {
-
+        if (message != null) {
+            queue.offer(message);
+        }
+        throw new IllegalArgumentException("Message cannot be null");
     }
 
     @Override
     public Message dequeue() {
-        return null;
+        if (!queue.isEmpty()) {
+            return queue.poll();
+        }
+        throw new IllegalArgumentException("Queue has no elements");
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return queue.size();
     }
 }
