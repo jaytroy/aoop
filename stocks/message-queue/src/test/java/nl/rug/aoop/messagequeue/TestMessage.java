a1826ff70ec2b8e14da0b9c8ed4f3ed86857b3e9
ohassaneRUG <ohassan100100@gmail.com>
2023-09-18 13:30:53 +0200

    Added more testing

--- stocks/message-queue/src/test/java/nl/rug/aoop/messagequeue/TestMessage.java
+++ stocks/message-queue/src/test/java/nl/rug/aoop/messagequeue/TestMessage.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -33,6 +35,54 @@ public class TestMessage {
         assertEquals(messageHeader, message.getHeader());
         assertEquals(messageBody, message.getBody());
         assertNotNull(message.getTimestamp());
+    }
+
+    @Test
+    void testTimeStamp() { //checks if the current message is within 1 second of creating this test
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime timestamp = message.getTimestamp();
+
+        LocalDateTime windowStart = now.minusSeconds(1);
+        LocalDateTime windowEnd = now.plusSeconds(1);
+
+        assertTrue(timestamp.isAfter(windowStart) && timestamp.isBefore(windowEnd));
+    }
+
+
+    @Test
+    void testNonEmptyHeaderAndBody() { // Test checks if either the header or body is empty
+        assertNotEquals("", message.getHeader());
+        assertNotEquals("", message.getBody());
+    }
+
+    @Test
+    void testMaxCharacter() {
+        String longHeader = "This is more than two hundred and eighty characters This is more than two hundred and " +
+                "eighty characters This is more than two hundred and eighty characters This is more than two hundred " +
+                "and eighty characters This is more than two hundred and eighty characters This is more than two " +
+                "hundred and eighty characters";
+        String longBody = "This is more than two hundred and eighty characters This is more than two hundred and " +
+                "eighty characters This is more than two hundred and eighty characters This is more than two hundred " +
+                "and eighty characters This is more than two hundred and eighty characters This is more than two " +
+                "hundred and eighty characters";
+        String shortHeader = "This is a short header";
+        String shortBody = "This is a short header";
+        String maxHeader = "This is the max amount of characters. This is the max amount of characters. This is the " +
+                "max amount of characters. This is the max amount of ";
+        String maxBody = "This is the max amount of characters. This is the max amount of characters. This is the " +
+                "max amount of characters. This is the max amount of ";
+
+        String greaterThanSixtyHeader = "Header should be a max of sixty character Header should be a max of sixty " +
+                "characters ";
+        String sixtyHeader = "Header should be a max of sixty characters Header should be ";
+        String lessThanSixtyHeader = "Header should be a max of sixty character";
+
+        assertTrue((longHeader.length() + longBody.length()) > 280);
+        assertTrue((shortHeader.length() + shortBody.length()) < 280);
+        assertEquals(280, maxHeader.length() + maxBody.length());
+        assertTrue(greaterThanSixtyHeader.length() > 60);
+        assertEquals(60, sixtyHeader.length());
+        assertTrue(lessThanSixtyHeader.length() < 60);
     }
 
     @Test
