dd6889ce601a7dbbbeecc6b6bb819d02b4077b10
ohassaneRUG <ohassan100100@gmail.com>
2023-10-10 16:14:33 +0200

    Added command tests

--- stocks/message-queue/src/test/java/nl/rug/aoop/messagequeue/testserverside/TestCommandMessageHandler.java
+++ stocks/message-queue/src/test/java/nl/rug/aoop/messagequeue/testserverside/TestCommandMessageHandler.java
@@ -1,4 +1,41 @@
 package nl.rug.aoop.messagequeue.testserverside;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import nl.rug.aoop.command.CommandHandler;
+import nl.rug.aoop.messagequeue.queues.Message;
+import nl.rug.aoop.messagequeue.serverside.commands.CommandMessageHandler;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestCommandMessageHandler {
+
+    private CommandMessageHandler commandMessageHandler;
+    private CommandHandler mockCommandHandler;
+
+    @BeforeEach
+    public void setUp() {
+        mockCommandHandler = Mockito.mock(CommandHandler.class);
+        commandMessageHandler = new CommandMessageHandler(mockCommandHandler);
+    }
+
+    @Test
+    public void testHandleMessage() {
+        Message message = new Message("testHeader", "testBody");
+        String messageJson = message.toJson();
+
+        // Define the expected parameters
+        Map<String, Object> expectedParams = new HashMap<>();
+        expectedParams.put("messageJson", messageJson);
+        expectedParams.put("header", "testHeader");
+        expectedParams.put("body", "testBody");
+        expectedParams.put("timestamp", message.getTimestamp());
+
+        // Call the handleMessage method
+        commandMessageHandler.handleMessage(messageJson);
+
+        // Verify that the commandHandler.executeCommand method was called with the expected parameters
+        Mockito.verify(mockCommandHandler).executeCommand("testHeader", expectedParams);
+    }
 }
--- stocks/message-queue/src/test/java/nl/rug/aoop/messagequeue/testserverside/TestMqPutCommand.java
+++ stocks/message-queue/src/test/java/nl/rug/aoop/messagequeue/testserverside/TestMqPutCommand.java
@@ -1,4 +1,35 @@
 package nl.rug.aoop.messagequeue.testserverside;
 
+import nl.rug.aoop.messagequeue.serverside.commands.MqPutCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import nl.rug.aoop.messagequeue.queues.Message;
+import nl.rug.aoop.messagequeue.queues.MessageQueue;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestMqPutCommand {
+
+    private MqPutCommand mqPutCommand;
+    private MessageQueue mockMessageQueue;
+
+    @BeforeEach
+    public void setUp() {
+        mockMessageQueue = Mockito.mock(MessageQueue.class);
+        mqPutCommand = new MqPutCommand(mockMessageQueue);
+    }
+
+    @Test
+    public void testExecute() {
+        Message message = new Message("testHeader", "testBody");
+        String messageJson = message.toJson();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("messageJson", messageJson);
+
+        mqPutCommand.execute(params);
+
+        Mockito.verify(mockMessageQueue).enqueue(message);
+    }
 }
