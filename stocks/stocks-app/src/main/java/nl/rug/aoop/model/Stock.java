0cf799bf66946acc236d25cec20610aaed0fd909
Jakub Borcel <j.borcel@student.rug.nl>
2023-10-26 15:37:57 +0200

    solving issues

--- stocks/stocks-app/src/main/java/nl/rug/aoop/model/Stock.java
+++ stocks/stocks-app/src/main/java/nl/rug/aoop/model/Stock.java
@@ -2,26 +2,32 @@ package nl.rug.aoop.model;
 
 public class Stock {
     private String symbol;
-    private double currentPrice;
+    private String name;
+    private long sharesOutstanding;
+    private double marketCap;
+    private double price;
+    private double initialPrice;
 
-    public Stock(String symbol, double currentPrice) {
-        this.symbol = symbol;
-        this.currentPrice = currentPrice;
+    public Stock() {
+        this.price = this.initialPrice;
     }
 
     public String getSymbol() {
         return symbol;
     }
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
+    public String getName() {
+        return name;
     }
 
+    public long getSharesOutstanding() {
+        return sharesOutstanding;
+    }
+
+    public double getMarketCap() {
+        return price * sharesOutstanding;
+    }
     public double getPrice() {
-        return currentPrice;
-    }
-
-    public void setPrice(double currentPrice) {
-        this.currentPrice = currentPrice;
+        return price;
     }
 }
