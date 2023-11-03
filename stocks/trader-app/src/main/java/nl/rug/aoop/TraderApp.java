790f740886770746c7bd33d32cfa4e4a24126cee
Jakub Borcel <j.borcel@student.rug.nl>
2023-11-03 15:34:23 +0100

    stuff

--- stocks/trader-app/src/main/java/nl/rug/aoop/TraderApp.java
+++ stocks/trader-app/src/main/java/nl/rug/aoop/TraderApp.java
@@ -18,40 +18,10 @@ public class TraderApp {
     public void initialize() {
         Trader trader1 = new Trader("bot1",getAdd());
 
-        BuyAndSell(trader1);
+        //BuyAndSell(trader1);
 
-/*
-        //I'm assuming most of this logic should be able to be moved out into the actual trader classes, or some
-        // utility classes
-        MessageHandler handler = new TraderHandler(); //Should this be done in the trader / client itself? Should each
-        // have its own?
-        int port;
-        int BACKUP_PORT = 8080;
-        InetSocketAddress address;
-        if(System.getenv("MESSAGE_QUEUE_PORT") != null) {
-            port = Integer.parseInt(System.getenv("MESSAGE_QUEUE_PORT"));
-        } else {
-            port = BACKUP_PORT;
-            System.out.println("Using backup port at TraderAppMain");
-        }
-        address = new InetSocketAddress("localhost", port);
-        Client client = new Client(handler, address);
-        try {
-            client.connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Thread clientThread = new Thread(client);
-        clientThread.start();
-
-        Trader jay = new Trader(client,"Jay", 0, 1000);
-        Message msg = new Message("PUT","BUY 10 AMD");
-        jay.putMessage(msg);
-
-        //jay.putMessage(null); Does not work
-        */
     }
-
+    /*
     private static void BuyAndSell(Trader trader1) {
         Random random = new Random();
         String[] stockSymbols = {"AAPL", "TSLA", "AMZN", "MSFT", "NVDA", "AMD", "ADBE", "FB", "INTC", "AOOP", "MRNA"};
@@ -77,7 +47,7 @@ public class TraderApp {
             }
         }
     }
-
+*/
     public InetSocketAddress getAdd() {
         int port;
         int BACKUP_PORT = 8080;
