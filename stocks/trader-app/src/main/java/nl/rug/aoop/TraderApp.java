fd9b6f6ac9d4077e3f7f7eee503099a1a9e7a5ed
Jakub Borcel <j.borcel@student.rug.nl>
2023-11-03 14:50:02 +0100

    random stuff

--- stocks/trader-app/src/main/java/nl/rug/aoop/TraderApp.java
+++ stocks/trader-app/src/main/java/nl/rug/aoop/TraderApp.java
@@ -1,15 +1,9 @@
 package nl.rug.aoop;
 
-import nl.rug.aoop.messagequeue.queues.Message;
-import nl.rug.aoop.networking.MessageHandler;
-import nl.rug.aoop.networking.client.Client;
-
-import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import java.net.InetSocketAddress;
 
-import static nl.rug.aoop.actions.Order.Type.BUY;
-import static nl.rug.aoop.actions.Order.Type.SELL;
-
+@Slf4j
 public class TraderApp {
     public void initialize() {
         Trader trader1 = new Trader("bot1",getAdd());
@@ -17,8 +11,10 @@ public class TraderApp {
         Trader trader3 = new Trader("bot3",getAdd());
 
 
-        //trader1.placeOrder(BUY, "AMD", 100, 1000);
         //trader2.placeOrder(SELL, "AMD", 10, 900);
+        //trader1.placeOrder(BUY, "AMD", 100, 950);
+        //trader3.placeOrder(SELL, "AMD", 50, 800);
+        //trader2.placeOrder(BUY, "AMD", 40, 850);
     }
 
     public InetSocketAddress getAdd() {
@@ -33,4 +29,33 @@ public class TraderApp {
         }
         return new InetSocketAddress("localhost", port);
     }
+
+    /*
+    private static void BuyAndSellStrat(Trader trader1) {
+        Random random = new Random();
+        String[] stockSymbols = {"AAPL", "TSLA", "AMZN", "MSFT", "NVDA", "AMD", "ADBE", "FB", "INTC", "AOOP", "MRNA"};
+
+        int numOrders = 10;
+        for (int i = 0; i < numOrders; i++) {
+            String randomStockSymbol = stockSymbols[random.nextInt(stockSymbols.length)];
+            int randomQuantityBuy = random.nextInt(100) + 1;;
+            double priceFactor = 1.0 + (0.01 * random.nextDouble());
+            double limitPriceBuy = stock.getPrice() * priceFactor;
+            double limitPriceSell = stock.getPrice() / priceFactor;
+            int buyOrSell = random.nextInt(2);
+
+            if (buyOrSell == 1) {
+                trader1.placeOrder(BUY, randomStockSymbol, randomQuantityBuy, limitPriceBuy);
+            } else {
+                trader1.placeOrder(SELL, randomStockSymbol,randomQuantityBuy, limitPriceSell);
+            }
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                log.error("Thread failed to sleep");
+            }
+        }
+    }
+     */
 }
+
