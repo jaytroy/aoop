5266d9fb01bf320f3d267b921d3b460c4e9315e0
ohassaneRUG <ohassan100100@gmail.com>
2023-11-25 15:09:47 +0100

    iMPLEMENTED market orders

--- stocks/trader-app/src/main/java/nl/rug/aoop/TraderStrategy.java
+++ stocks/trader-app/src/main/java/nl/rug/aoop/TraderStrategy.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import static nl.rug.aoop.actions.Order.Action.BUY;
 import static nl.rug.aoop.actions.Order.Action.SELL;
 import static nl.rug.aoop.actions.Order.Type.LIMIT;
+import static nl.rug.aoop.actions.Order.Type.MARKET;
 
 /**
  * The TraderStrategy class represents the strategy for generating random orders for a trader.
@@ -30,8 +31,38 @@ public class TraderStrategy {
      * Implement a trader strategy for generating random orders.
      */
     public void executeStrategy() {
-        buySellStrategy();
+        Random random = new Random();
+        int marketOrLimit = random.nextInt(1);
+        buySellMarketStrategy();
         //more strategies here as needed
+    }
+
+    private void buySellMarketStrategy() {
+        Random random = new Random();
+        // For buying, get all possible stock symbols from available stocks
+        List<String> stockSymbolsToBuy = traderFacade.getTrader().getAvailableStocks().stream().map(Stock::getSymbol).
+                toList();
+        // For selling, get stock symbols from the symbols of owned stocks
+        List<String> stockSymbolsToSell = traderFacade.getTrader().getOwnedStocks().keySet().stream().toList();
+        // For buying, randomly choose a stock symbol from available stocks
+        String randomStockSymbolBuy = stockSymbolsToBuy.get(random.nextInt(stockSymbolsToBuy.size()));
+        // For selling, randomly choose a stock symbol from owned stocks
+        String randomStockSymbolSell = stockSymbolsToSell.get(random.nextInt(stockSymbolsToSell.size()));
+        // Choose the correct stock price from available stocks
+        Stock chosenStock = traderFacade.getTrader().getAvailableStocks().stream()
+                .filter(stock -> stock.getSymbol().equals(randomStockSymbolBuy))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Stock not found for symbol: " + randomStockSymbolBuy));
+        int maxQuantitySell = traderFacade.getTrader().getOwnedStocks().get(randomStockSymbolSell);
+        int randomQuantitySell = random.nextInt(maxQuantitySell) + 1;
+        int randomQuantityBuy = random.nextInt(100) + 1;
+        double price = chosenStock.getPrice();
+        int buyOrSell = random.nextInt(2);
+        if (buyOrSell == 1 && traderFacade.getTrader().getAvailableFunds() > 0) {
+            traderFacade.placeOrder(BUY, MARKET, randomStockSymbolBuy, randomQuantityBuy, price);
+        } else {
+            traderFacade.placeOrder(SELL, MARKET, randomStockSymbolSell, randomQuantitySell, price);
+        }
     }
 
     private void buySellStrategy() {
